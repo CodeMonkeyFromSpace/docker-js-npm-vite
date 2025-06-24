0be038bd61545270b68c0d8522bfbae24a3f6822
Paul Abrams <john.paul.abrams@gmail.com>
2025-06-24 17:05:00 -0400

    feat: sonnet implementation

--- src/lexismallestStringWithinCost-stub.js
+++ src/lexismallestStringWithinCost-stub.js
@@ -4,9 +4,34 @@ The goal is to remove characters from S to make it be as soon in a dictionary as
 
 Assume that the string can only contain zero or more chars in the range a-z.
 */
-
+// src/lexismallestStringWithinCost-stub.js
 export function lexismallestStringWithinCost(s, costs, k) {
-    let best = s;
-    // paste implementation here.
-    return best;
+    const n = s.length;
+    let result = [];
+    let remainingCost = k;
+    
+    for (let i = 0; i < n; i++) {
+        // While we have characters in result and can afford to remove the last one
+        // and the current character is smaller than the last one in result
+        while (result.length > 0 && 
+               remainingCost >= costs[result[result.length - 1]] &&
+               s[i] < s[result[result.length - 1]]) {
+            // Remove the last character and add its cost back
+            const removedIndex = result.pop();
+            remainingCost += costs[removedIndex];
+        }
+        
+        // Add current character to result
+        result.push(i);
+    }
+    
+    // Now we have the best possible arrangement, but we might still be able to
+    // remove some characters from the end to reduce the string further
+    while (result.length > 0 && remainingCost >= costs[result[result.length - 1]]) {
+        const removedIndex = result.pop();
+        remainingCost += costs[removedIndex];
+    }
+    
+    // Build the final string from the remaining indices
+    return result.map(index => s[index]).join('');
 }

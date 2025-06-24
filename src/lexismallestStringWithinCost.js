f4965f42cbdb29359f4138275bb03ed420468a47
Paul Abrams <john.paul.abrams@gmail.com>
2025-06-24 15:27:02 -0400

    chore: clean up

--- src/lexismallestStringWithinCost.js
+++ src/lexismallestStringWithinCost.js
@@ -1,32 +1,19 @@
 export function lexismallestStringWithinCost(s, costs, k) {
-    /**
-     * Remove characters to get lexicographically smallest string within cost budget.
-     * Uses brute force approach to handle negative costs correctly.
-     * 
-     * @param {string} s - Input string
-     * @param {number[]} costs - Cost to remove each character (can be negative)
-     * @param {number} k - Maximum total cost allowed
-     * @returns {string} Lexicographically smallest string within budget
-     */
     const n = s.length;
     let bestResult = s;
     
     for (let mask = 0; mask < (1 << n); mask++) {
         let totalCost = 0;
         let resultString = '';
-        let removed = [];
         
         for (let i = 0; i < n; i++) {
             if ((mask >> i) & 1) {
-                // Bit i is set: remove character i
                 totalCost += costs[i];
-                removed.push(i);
             } else {
-                // Bit i is not set: keep character i
                 resultString += s[i];
             }
         }
-
+        
         if (totalCost <= k && resultString < bestResult) {
             bestResult = resultString;
         }
--- test/lexismallestStringWithinCost.test.js
+++ test/lexismallestStringWithinCost.test.js
@@ -1,3 +1,10 @@
+/*
+We have a string of length S and an array of integers of length S, and an integer C.
+The goal is to remove characters from S to make it be as soon in a dictionary as possible. The soonest possible is the empty string. Obviously, that would be the answer every time if we could just remove all the chars we want. But the catch is that every time we remove a character from the string, it "costs" the value of the integer at the corresponding index of the input array. The total cost cannot be more than the input integer C. So we have to find the alphabetically smallest string by removing characters from S, but not exceeding cost C.
+
+Assume that the string can only contain zero or more chars in the range a-z.
+*/
+
 import { lexismallestStringWithinCost as ImplA } from '../src/lexismallestStringWithinCost.js';
 
 const testCases = [
@@ -60,6 +67,16 @@ const testCases = [
     input: ["cab", [10, 1, 1], 2],
     expected: "c",
     description: 'high initial cost',
+  },
+  {
+    input: ["dxaayzbc", [25, 4, 4, -5, 1, 1, 1, 1], 3],
+    expected: "da",
+    description: 'dxaayzbc',
+  },
+  {
+    input: ["zazazz", [11, -1, 3, -3, -4, 1], 3],
+    expected: "z",
+    description: 'zazazz',
   }
 ];
 
@@ -74,4 +91,3 @@ function runTests(impl, name) {
 }
 
 runTests(ImplA, 'Implementation A');
-// runTests(ImplB, 'Implementation B');
